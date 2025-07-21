eec8f4ef326c51bc80ff36608882259c4dc85b9b
lbinria <77280433+lbinria@users.noreply.github.com>
2025-07-21 16:33:05 +0200

    feat: Refactor inspector script

--- modules/module_inspector/script.lua
+++ modules/module_inspector/script.lua
@@ -1,47 +1,26 @@
+function get_str(primitive_state)
+	local n_hovered = #primitive_state.all_hovered
+	local str_hovered_single = "X"
+	local str_hovered_many = "X"
+	if primitive_state.any_hovered then 
+		str_hovered_single = tostring(primitive_state.hovered)
+		for i = 1, n_hovered do 
+			if i == 1 then
+				str_hovered_many = tostring(primitive_state.all_hovered[i])
+			else
+				str_hovered_many = str_hovered_many .. ", " .. tostring(primitive_state.all_hovered[i])
+			end
+		end
+	end
+	return n_hovered, str_hovered_single, str_hovered_many
+end
+
 function draw_gui() 
 	imgui.Begin("Inspector")
 
-	local n_hovered_cells = #app.input_state.cell.all_hovered
-	local str_hovered_cell = "X"
-	local str_hovered_cells = "X"
-	if app.input_state.cell.any_hovered then 
-		str_hovered_cell = tostring(app.input_state.cell.hovered)
-		for i = 1, n_hovered_cells do 
-			if i == 1 then
-				str_hovered_cells = tostring(app.input_state.cell.all_hovered[i])
-			else
-				str_hovered_cells = str_hovered_cells .. ", " .. tostring(app.input_state.cell.all_hovered[i])
-			end
-		end
-	end
-
-	local n_hovered_facets = #app.input_state.facet.all_hovered
-	local str_hovered_facet = "X"
-	local str_hovered_facets = "X"
-	if app.input_state.facet.any_hovered then 
-		str_hovered_facet = tostring(app.input_state.facet.hovered)
-		for i = 1, n_hovered_facets do 
-			if i == 1 then
-				str_hovered_facets = tostring(app.input_state.facet.all_hovered[i])
-			else
-				str_hovered_facets = str_hovered_facets .. ", " .. tostring(app.input_state.facet.all_hovered[i])
-			end
-		end
-	end
-
-	local n_hovered_vertices = #app.input_state.vertex.all_hovered
-	local str_hovered_vertex = "X"
-	local str_hovered_vertices = "X"
-	if app.input_state.vertex.any_hovered then 
-		str_hovered_vertex = tostring(app.input_state.vertex.hovered)
-		for i = 1, n_hovered_vertices do 
-			if i == 1 then
-				str_hovered_vertices = tostring(app.input_state.vertex.all_hovered[i])
-			else
-				str_hovered_vertices = str_hovered_vertices .. ", " .. tostring(app.input_state.vertex.all_hovered[i])
-			end
-		end
-	end
+	local n_hovered_cells, str_hovered_cell, str_hovered_cells = get_str(app.input_state.cell)
+	local n_hovered_facets, str_hovered_facet, str_hovered_facets = get_str(app.input_state.facet)
+	local n_hovered_vertices, str_hovered_vertex, str_hovered_vertices = get_str(app.input_state.vertex)
 
 	local str_hovered_edge = "X"
 	if app.input_state.edge.any_hovered then 
