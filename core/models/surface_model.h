860d0f23405b97d8e2d2fb52a511f9d6afd672ae
lbinria <77280433+lbinria@users.noreply.github.com>
2025-12-17 11:30:45 +0100

    chore: Refactor generic functions on surface models to `SurfaceModel`

---
+++ core/models/surface_model.h
@@ -0,0 +1,165 @@
+#pragma once
+
+#include "../../include/json.hpp"
+#include <ultimaille/all.h>
+#include <string>
+#include "model.h"
+#include "../renderers/point_set_renderer.h"
+#include "../renderers/halfedge_renderer.h"
+#include "../renderers/tri_renderer.h"
+#include "../renderers/bbox_renderer.h"
+#include "../renderers/clipping_renderer.h"
+#include "../color_mode.h"
+#include "../layer.h"
+#include "../helpers.h"
+
+struct SurfaceModel : public Model {
+
+	SurfaceModel(std::map<std::string, std::shared_ptr<IRenderer>> renderers) :
+	Model::Model(renderers) {}
+
+	SurfaceModel(std::map<std::string, std::shared_ptr<IRenderer>> renderers, std::string name) : 
+	Model::Model(renderers, name) {}
+
+	std::tuple<glm::vec3, glm::vec3> bbox() override {
+		auto &m = getSurface();
+
+		glm::vec3 min = glm::vec3(FLT_MAX);
+		glm::vec3 max = glm::vec3(-FLT_MAX);
+
+		for (auto &v : m.iter_vertices()) {
+			glm::vec3 p = sl::um2glm(v.pos());
+			min = glm::min(min, p);
+			max = glm::max(max, p);
+		}
+
+		return {min, max};
+	}
+
+	SurfaceAttributes& getSurfaceAttributes() { return _surfaceAttributes; }
+	const SurfaceAttributes& getSurfaceAttributes() const { return _surfaceAttributes; }
+
+    int nverts() const override {
+		auto &m = getSurface();
+        return m.nverts();
+    }
+
+    int nfacets() const override {
+		auto &m = getSurface();
+        return m.nfacets();
+    } 
+
+    int ncells() const override {
+        return 0;
+    }
+
+    int ncorners() const override {
+		auto &m = getSurface();
+        return m.ncorners();
+    }
+
+	int nhalfedges() const override {
+		auto &m = getSurface();
+		return m.ncorners();
+	}
+
+    long pick_edge(glm::vec3 p0, int f) override {
+		auto &m = getSurface();
+
+        // Search nearest edge
+        double min_d = std::numeric_limits<double>().max();
+        long found_e = -1;
+        
+		auto fc = m.facet(f);
+		int size = fc.size();
+
+        for (int lv = 0; lv < size; ++lv) {
+            
+        	// Get global indices of vertex on edge extremities
+        	auto v0 = fc.vertex(lv % size);
+        	auto v1 = fc.vertex((lv + 1) % size);
+
+        	// Get points from current edge
+        	vec3 p1 = m.points[v0];
+        	vec3 p2 = m.points[v1];
+        	vec3 b = (p1 + p2) * .5;
+        	// Compute dist from picked point to bary of edge points
+        	double d = (vec3(p0.x, p0.y, p0.z) - b).norm(); // TODO maybe use norm2 will give the same result
+
+        	// Keep min dist
+        	if (d < min_d) {
+        		min_d = d;
+        		found_e = f * size + lv;
+        	}
+        }
+
+        return found_e;
+    }
+
+	protected:
+
+	SurfaceAttributes _surfaceAttributes;
+
+	virtual Surface& getSurface() = 0;
+	virtual const Surface& getSurface() const = 0;
+
+	// Maybe move to surfacemodel ? eventually merge with setLayer
+	void updateLayer(Layer layer, ElementKind kind) override {
+
+		auto attrName = getLayerAttr(layer, kind);
+
+		std::optional<std::vector<double>> data_opt;
+
+		switch (kind) {
+			case ElementKind::POINTS_ELT: {
+				data_opt = getAttrData<PointAttribute<double>>(attrName);
+				break;
+			}
+			case ElementKind::CORNERS_ELT:
+			case ElementKind::EDGES_ELT: {
+				data_opt = getAttrData<CornerAttribute<double>>(attrName);
+				break;
+			}
+			case ElementKind::FACETS_ELT: {
+				data_opt = getAttrData<FacetAttribute<double>>(attrName);
+				break;
+			}
+			default:
+				std::cerr << "Warning: Model::updateLayer() on " 
+					<< elementKindToString(kind) 
+					<< " of "
+					<< modelTypeToString(getModelType())
+					<< " with layer " 
+					<< layerToString(layer) 
+					<< " is not supported.." << std::endl;
+				return;
+		}
+
+		if (!data_opt.has_value())
+			return;
+
+		// double to float
+		std::vector<float> data(data_opt.value().size());
+		std::transform(data_opt.value().begin(),data_opt.value().end(), data.begin(), [](auto v) { return static_cast<float>(v); });
+
+		for (auto const &[k, r] : _renderers) {
+			if (r->isRenderElement(kind)) {
+				r->setLayer(data, layer);
+			}
+		}
+	}
+
+
+
+	private:
+
+	template<typename T> 
+	std::optional<std::vector<double>> getAttrData(std::string attrName) {
+		T layerAttr;
+		if (!layerAttr.bind(attrName, _surfaceAttributes, getSurface()))
+			return std::nullopt;
+
+		return layerAttr.ptr->data;
+	}
+
+};
--- core/models/tri_model.h
+++ core/models/tri_model.h
@@ -3,7 +3,7 @@
 #include "../../include/json.hpp"
 #include <ultimaille/all.h>
 #include <string>
-#include "model.h"
+#include "surface_model.h"
 #include "../renderers/point_set_renderer.h"
 #include "../renderers/halfedge_renderer.h"
 #include "../renderers/tri_renderer.h"
@@ -16,11 +16,11 @@
 using namespace UM;
 using json = nlohmann::json;
 
-struct TriModel final : public Model {
+struct TriModel final : public SurfaceModel {
 
 	TriModel() : 
         _m(),
-        Model::Model({
+        SurfaceModel::SurfaceModel({
             {"mesh_renderer", std::make_shared<TriRenderer>(_m)}, 
             {"point_renderer", std::make_shared<PointSetRenderer>(_m.points) },
             {"edge_renderer", std::make_shared<SurfaceHalfedgeRenderer>(_m) },
@@ -40,128 +40,129 @@ struct TriModel final : public Model {
 
 
 	Triangles& getTriangles() { return _m; }
-	SurfaceAttributes& getSurfaceAttributes() { return _surfaceAttributes; }
+    Surface& getSurface() override { return _m; }
+    const Surface& getSurface() const override { return _m; }
 
-    int nverts() const override {
-        return _m.nverts();
-    }
+    // int nverts() const override {
+    //     return _m.nverts();
+    // }
 
-    int nfacets() const override {
-        return _m.nfacets();
-    } 
+    // int nfacets() const override {
+    //     return _m.nfacets();
+    // } 
 
-    int ncells() const override {
-        return 0;
-    }
+    // int ncells() const override {
+    //     return 0;
+    // }
 
-    int ncorners() const override {
-        return _m.ncorners();
-    }
+    // int ncorners() const override {
+    //     return _m.ncorners();
+    // }
 
-	int nhalfedges() const override {
-		return _m.ncorners();
-	}
+	// int nhalfedges() const override {
+	// 	return _m.ncorners();
+	// }
 
-    std::tuple<glm::vec3, glm::vec3> bbox() override {
-        glm::vec3 min = glm::vec3(FLT_MAX);
-        glm::vec3 max = glm::vec3(-FLT_MAX);
+    // std::tuple<glm::vec3, glm::vec3> bbox() override {
+    //     glm::vec3 min = glm::vec3(FLT_MAX);
+    //     glm::vec3 max = glm::vec3(-FLT_MAX);
 
-        for (auto &v : _m.iter_vertices()) {
-            glm::vec3 p = sl::um2glm(v.pos());
-            min = glm::min(min, p);
-            max = glm::max(max, p);
-        }
+    //     for (auto &v : _m.iter_vertices()) {
+    //         glm::vec3 p = sl::um2glm(v.pos());
+    //         min = glm::min(min, p);
+    //         max = glm::max(max, p);
+    //     }
 
-        return {min, max};
-    }
+    //     return {min, max};
+    // }
 
-    long pick_edge(glm::vec3 p0, int f) override {
-        // Search nearest edge
-        double min_d = std::numeric_limits<double>().max();
-        long found_e = -1;
+    // long pick_edge(glm::vec3 p0, int f) override {
+    //     // Search nearest edge
+    //     double min_d = std::numeric_limits<double>().max();
+    //     long found_e = -1;
         
-        for (int lv = 0; lv < 3; ++lv) {
+    //     for (int lv = 0; lv < 3; ++lv) {
             
-        	// Get global indices of vertex on edge extremities
-        	auto v0 = _m.facet(f).vertex(lv % 3);
-        	auto v1 = _m.facet(f).vertex((lv + 1) % 3);
+    //     	// Get global indices of vertex on edge extremities
+    //     	auto v0 = _m.facet(f).vertex(lv % 3);
+    //     	auto v1 = _m.facet(f).vertex((lv + 1) % 3);
 
-        	// Get points from current edge
-        	vec3 p1 = _m.points[v0];
-        	vec3 p2 = _m.points[v1];
-        	vec3 b = (p1 + p2) * .5;
-        	// Compute dist from picked point to bary of edge points
-        	double d = (vec3(p0.x, p0.y, p0.z) - b).norm(); // TODO maybe use norm2 will give the same result
+    //     	// Get points from current edge
+    //     	vec3 p1 = _m.points[v0];
+    //     	vec3 p2 = _m.points[v1];
+    //     	vec3 b = (p1 + p2) * .5;
+    //     	// Compute dist from picked point to bary of edge points
+    //     	double d = (vec3(p0.x, p0.y, p0.z) - b).norm(); // TODO maybe use norm2 will give the same result
 
-        	// Keep min dist
-        	if (d < min_d) {
-        		min_d = d;
-        		found_e = f * 3 + lv;
-        	}
-        }
+    //     	// Keep min dist
+    //     	if (d < min_d) {
+    //     		min_d = d;
+    //     		found_e = f * 3 + lv;
+    //     	}
+    //     }
 
-        return found_e;
-    }
+    //     return found_e;
+    // }
 
     private: 
 
     // Mesh
     Triangles _m;
-    SurfaceAttributes _surfaceAttributes;
+    // SurfaceAttributes _surfaceAttributes;
 
-    template<typename T> 
-    std::optional<std::vector<double>> getAttrData(std::string attrName) {
-        T layerAttr;
-        if (!layerAttr.bind(attrName, _surfaceAttributes, _m))
-            return std::nullopt;
+    // template<typename T> 
+    // std::optional<std::vector<double>> getAttrData(std::string attrName) {
+    //     T layerAttr;
+    //     if (!layerAttr.bind(attrName, _surfaceAttributes, _m))
+    //         return std::nullopt;
 
-        return layerAttr.ptr->data;
-    }
+    //     return layerAttr.ptr->data;
+    // }
 
-    // Maybe move to surfacemodel ? eventually merge with setLayer
-    void updateLayer(Layer layer, ElementKind kind) override {
+    // // Maybe move to surfacemodel ? eventually merge with setLayer
+    // void updateLayer(Layer layer, ElementKind kind) override {
 
-        auto attrName = getLayerAttr(layer, kind);
+    //     auto attrName = getLayerAttr(layer, kind);
 
-        std::optional<std::vector<double>> data_opt;
+    //     std::optional<std::vector<double>> data_opt;
 
-        switch (kind) {
-            case ElementKind::POINTS_ELT: {
-                data_opt = getAttrData<PointAttribute<double>>(attrName);
-                break;
-            }
-            case ElementKind::CORNERS_ELT:
-            case ElementKind::EDGES_ELT: {
-                data_opt = getAttrData<CornerAttribute<double>>(attrName);
-                break;
-            }
-            case ElementKind::FACETS_ELT: {
-                data_opt = getAttrData<FacetAttribute<double>>(attrName);
-                break;
-            }
-            default:
-                std::cerr << "Warning: Model::updateLayer() on " 
-                    << elementKindToString(kind) 
-                    << " of "
-                    << modelTypeToString(getModelType())
-                    << " with layer " 
-                    << layerToString(layer) 
-                    << " is not supported.." << std::endl;
-                return;
-        }
+    //     switch (kind) {
+    //         case ElementKind::POINTS_ELT: {
+    //             data_opt = getAttrData<PointAttribute<double>>(attrName);
+    //             break;
+    //         }
+    //         case ElementKind::CORNERS_ELT:
+    //         case ElementKind::EDGES_ELT: {
+    //             data_opt = getAttrData<CornerAttribute<double>>(attrName);
+    //             break;
+    //         }
+    //         case ElementKind::FACETS_ELT: {
+    //             data_opt = getAttrData<FacetAttribute<double>>(attrName);
+    //             break;
+    //         }
+    //         default:
+    //             std::cerr << "Warning: Model::updateLayer() on " 
+    //                 << elementKindToString(kind) 
+    //                 << " of "
+    //                 << modelTypeToString(getModelType())
+    //                 << " with layer " 
+    //                 << layerToString(layer) 
+    //                 << " is not supported.." << std::endl;
+    //             return;
+    //     }
 
-        if (!data_opt.has_value())
-            return;
+    //     if (!data_opt.has_value())
+    //         return;
 
-        // double to float
-        std::vector<float> data(data_opt.value().size());
-        std::transform(data_opt.value().begin(),data_opt.value().end(), data.begin(), [](auto v) { return static_cast<float>(v); });
+    //     // double to float
+    //     std::vector<float> data(data_opt.value().size());
+    //     std::transform(data_opt.value().begin(),data_opt.value().end(), data.begin(), [](auto v) { return static_cast<float>(v); });
 
-        for (auto const &[k, r] : _renderers) {
-            if (r->isRenderElement(kind)) {
-                r->setLayer(data, layer);
-            }
-        }
-    }
+    //     for (auto const &[k, r] : _renderers) {
+    //         if (r->isRenderElement(kind)) {
+    //             r->setLayer(data, layer);
+    //         }
+    //     }
+    // }
 
 };
