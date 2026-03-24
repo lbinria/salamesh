1c031b5bc6b456458e7fc306b5a66cda86b041bf
lbinria <77280433+lbinria@users.noreply.github.com>
2026-03-24 13:42:25 +0100

    fix: Remove useless includes in `render_surface.cpp`

--- core/render_surface.h
+++ core/render_surface.h
@@ -8,47 +8,47 @@
 #include <memory>
 
 struct RenderSurface {
-    
-    RenderSurface(int w, int h) : width(w), height(h) {
+	
+	RenderSurface(int w, int h) : width(w), height(h) {
 
-        
-    }
+		
+	}
 
 	unsigned int fbo;
-    unsigned int rbo;
-    
-    unsigned int depthAttachmentTexture;
-    unsigned int texColor;
-    unsigned int texCellID;
-    unsigned int texFacetID;
-    unsigned int texVertexID;
-    unsigned int texMeshID;
+	unsigned int rbo;
+	
+	unsigned int depthAttachmentTexture;
+	unsigned int texColor;
+	unsigned int texCellID;
+	unsigned int texFacetID;
+	unsigned int texVertexID;
+	unsigned int texMeshID;
 
 	int width, height;
-    glm::vec3 backgroundColor;
+	glm::vec3 backgroundColor;
 
 
 	void setup();
-    void bind();
-    void clear();
-    void render(Shader &screenShader, unsigned int quadVAO);
-    void resize(int w, int h);
-    void clean();
+	void bind();
+	void clear();
+	void render(Shader &screenShader, unsigned int quadVAO);
+	void resize(int w, int h);
+	void clean();
 
-    // Add picking functions
+	// Add picking functions
 
-    void setCamera(std::shared_ptr<Camera> camera) {
-        _camera = camera;
-        _camera->updateScreenSize(width, height);
-    }
+	void setCamera(std::shared_ptr<Camera> camera) {
+		_camera = camera;
+		_camera->updateScreenSize(width, height);
+	}
 
-    void setBackgroundColor(glm::vec3 color) {
-        backgroundColor = color;
-    }
+	void setBackgroundColor(glm::vec3 color) {
+		backgroundColor = color;
+	}
 
-    Camera& getCamera() { return *_camera; }
+	Camera& getCamera() { return *_camera; }
 
-    private:
-    std::shared_ptr<Camera> _camera;
+	private:
+	std::shared_ptr<Camera> _camera;
 
 };
