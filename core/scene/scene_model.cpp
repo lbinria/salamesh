af1eddb04eb1a0e7efa7a1e58a0cf6041c169e93
lbinria <77280433+lbinria@users.noreply.github.com>
2026-08-06 15:10:13 +0200

    chore: Clean up

--- core/scene/scene_model.cpp
+++ core/scene/scene_model.cpp
@@ -27,53 +27,6 @@ void SceneModel::setColormap(Colormap colormap) {
 	return l.setColormap(colormap);
 }
 
-// // Note: it return the first colormap found (model can have heterogeneous colormaps between materials)
-// std::optional<Colormap> SceneModel::getColormap() {
-		
-// 	for (auto &[_, material] : getMaterials()) {
-// 		auto layerParams = material.getParams<ColormapLayerParams>("colormap");
-// 		if (!layerParams)
-// 			continue;
-		
-// 		return layerParams->getColormap(ColormapLayer::COLORMAP_LAYER_0);
-// 	}
-
-// 	return std::nullopt;
-// }
-
-
-// void SceneModel::setColormap(Colormap colormap) {
-
-// 	for (auto &[_, material] : getMaterials()) {
-// 		auto layerParams = material.getParams<ColormapLayerParams>("colormap");
-// 		if (!layerParams)
-// 			continue;
-		
-// 		layerParams->setColormap(ColormapLayer::COLORMAP_LAYER_0, colormap);
-// 	}
-
-// }
-
-// std::string SceneModel::getLayerAttr(Layer layer, ElementKind kind) {
-// 	std::tuple<Layer, ElementKind> k = {layer, kind};
-// 	if (_attrNameByLayerAndKind.contains(k))
-// 		return _attrNameByLayerAndKind[k];
-	
-
-// 	// return std::format("{}.{}{}",
-// 	// 	elementKindToString(kind),
-// 	// 	defaultAttrName(layer),
-// 	// 	getNDims() > 1 && dim >= 0 ? std::format("[{}]", dim) : "");
-
-// 	// Below old version
-// 	// return defaultAttrName(layer);
-
-// 	// TODO maybe not necessary anymore
-// 	return std::format("{}.{}",
-// 		elementKindToString(kind),
-// 		defaultAttrName(layer));
-// }
-
 std::optional<Attribute> SceneModel::getSelectedAttribute() {
 	return _selectedAttribute;
 }
@@ -89,31 +42,6 @@ void SceneModel::setSelectedAttribute(std::optional<Attribute> attr) {
 	_selectedAttribute = attr;
 }
 
-// void SceneModel::setSelectedAttribute(std::optional<Attribute> attr) {
-// 	// Unset previous colormap layer for the whole model
-// 	unsetLayers(Layer::COLORMAP_0);
-
-// 	if (attr.has_value())
-// 		setLayer(Layer::COLORMAP_0, attr.value(), true);
-	
-// 	_selectedAttribute = attr;
-// }
-
-// void SceneModel::setLayer(Layer layer, Attribute attr, bool update) {
-
-// 	_attrNameByLayerAndKind[{layer, attr.getKind()}] = attr.getFullName();
-
-// 	for (auto &[_, material] : getMaterials()) {
-// 		auto layerParams = material.getParams<MyLayerParams>(layerToString(layer)); 
-
-// 		if (layerParams)
-// 			layerParams->setAttribute(attr, true, update);
-// 	}
-// }
-
-
-
-
 void SceneModel::updateLayers() {
 	auto &mesh = getMesh();
 
@@ -122,44 +50,6 @@ void SceneModel::updateLayers() {
 		layers[layer].update();
 	}
 }
-
-// void SceneModel::unsetLayers(bool reset) {
-// 	for (int l = 0; l < 5; ++l) {
-// 		for (int k = 0; k < 7; ++k) {
-// 			unsetLayer(static_cast<Layer>(l), static_cast<ElementKind>(k), reset);
-// 		}
-// 	}
-// }
-
-// void SceneModel::unsetLayers(Layer layer, bool reset) {
-// 	for (int k = 0; k < 7; ++k) {
-// 		unsetLayer(layer, static_cast<ElementKind>(k), reset);
-// 	}
-// }
-
-// void SceneModel::unsetLayers(ElementKind kind, bool reset) {
-// 	for (int l = 0; l < 5; ++l) {
-// 		unsetLayer(static_cast<Layer>(l), kind, reset);
-// 	}
-// }
-
-// void SceneModel::unsetLayer(Layer layer, ElementKind kind, bool reset) {
-	
-// 	for (auto &[_, material] : getMaterials()) {
-// 		auto layerParams = material.getParams<MyLayerParams>(layerToString(layer));
-
-// 		if (!layerParams)
-// 			continue;
-
-// 		// Little optimisation, doesn't update data
-// 		// if layer isn't activated, no need to unset
-// 		if (!layerParams->isActivatedLayer(kind))
-// 			continue;
-		
-// 		layerParams->deactivateLayer(kind);
-// 	}
-
-// }
 
 void SceneModel::loadState(json &j, const std::string filename) {
 
@@ -212,25 +102,6 @@ void SceneModel::applyMaterialsFrom(SceneModel &model) {
 		// Try to set source material to target material
 		targetMaterialOpt.value().get().set(sourceMaterial);
 	}
-
-	// // Set layers
-	// for (int k = 0; k < static_cast<int>(ElementKind::ELEMENT_KIND_COUNT); ++k) {
-	// 	for (int l = 0; l < static_cast<int>(Layer::LAYER_COUNT); ++l) {
-
-	// 		auto layer = static_cast<Layer>(l);
-	// 		auto kind = static_cast<ElementKind>(k);
-
-	// 		if (!model._attrNameByLayerAndKind.contains({layer, kind}))
-	// 			continue;
-
-	// 		auto attrName = model._attrNameByLayerAndKind[{layer, kind}];
-
-	// 		if (attrName.empty())
-	// 			continue;
-			
-
-	// 	}
-	// }
 
 	// Apply selected attr if possible
 	// If source has same attribute than target, select !
